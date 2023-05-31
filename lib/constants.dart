d890887b742fce8768878fa8bf22853155030a66
mohamadreza <mohamadrezamohammady2019@gmail.com>
2023-05-31 10:19:19 -0700

    constants saved in a dart file

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const labelStyle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF868892),
+);
+
+const buttomContainerColor = Color(0xFFEB1555);
+const activeCardColor = Color(0xFF111428);
+const inActiveCardColor = Color(0xFF1D1F33);
+
+const numberStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+
+const LargeTextButtonStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
+
+const TitleTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+const ResultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontSize: 22, fontWeight: FontWeight.bold);
+const BmiTextStyle = TextStyle(fontSize: 100, fontWeight: FontWeight.bold);
+const BodyTextStyle = TextStyle(fontSize: 22);
